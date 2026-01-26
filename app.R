c757e728c6ecd1e03b87e86733d6d264765ca9dd
cacostakis <cacostakis@gmail.com>
2026-01-25 16:38:24 -0800

    Clean start app

---
+++ app.R
@@ -0,0 +1,81 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+library(shiny)
+library(viridis)
+library(tidyverse)
+library(ggiraph)
+library(sf)
+
+fam_per_country <- 
+  read_rds("families_per_countries.rds")
+
+static_df <- fam_per_country %>% select(-family)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Major African Language Families"),
+    theme = shinytheme("darkly"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons(label = "Select language family",
+                         inputId = "family",
+                         choices = unique(fam_per_country$family) %>% str_trim())
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           girafeOutput("geo_sf_plot"),
+           p("Data source: ", 
+             a("Wikipedia - Languages of Africa", 
+               href = "https://en.wikipedia.org/wiki/Languages_of_Africa"))
+           )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # Create reactive df that responds to the selected family
+  sf_interactive_df <- 
+    reactive({
+      fam_per_country %>% filter(family == input$family)
+    })
+  # Create the static color
+  map_fill_static_color <- viridis_pal(option = "D", begin = 0, end = 0)(1)
+  
+
+    output$geo_sf_plot <- renderGirafe({
+      
+      p <-
+        ggplot(data = sf_interactive_df()) +
+        geom_sf(data = static_df,
+                mapping = aes(), fill = map_fill_static_color, show.legend = F) +
+        geom_sf_interactive(aes(
+          tooltip = paste0(country,"- ", n, " languages \n\nList:\n", str_wrap(languages,45)),
+          fill = n,
+          data_id = country),
+          show.legend = T) +
+        scale_fill_viridis_c(trans = "sqrt", 
+                             name = "Number of\nfamilies",
+                             begin = 0.1,
+                             labels = scales::label_number(accuracy = 1)) +
+        facet_wrap(~family) +
+        theme_void()
+      
+      girafe(ggobj = p)
+      
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
