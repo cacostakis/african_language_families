2f5b5ccf0ce54167c1bf83c04359217e7c45faaa
cacostakis <cacostakis@gmail.com>
2026-01-31 15:51:58 -0800

    styling with plotly

--- app.R
+++ app.R
@@ -13,6 +13,8 @@ library(tidyverse)
 library(ggiraph)
 library(sf)
 library(shinythemes)
+library(plotly)
+
 
 fam_per_country <- 
   read_rds("families_per_countries.rds")
@@ -36,7 +38,8 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           girafeOutput("geo_sf_plot"),
+          div(textOutput("family_title"), style = "text-align: center;"),
+           plotlyOutput("geo_sf_plot"),
            p("Data source: ", 
              a("Wikipedia - Languages of Africa", 
                href = "https://en.wikipedia.org/wiki/Languages_of_Africa"))
@@ -47,6 +50,11 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  # Titling
+  output$family_title <- renderText({
+    paste(input$family, "language family")
+  })
+  
   # Create reactive df that responds to the selected family
   sf_interactive_df <- 
     reactive({
@@ -56,25 +64,27 @@ server <- function(input, output) {
   map_fill_static_color <- viridis_pal(option = "D", begin = 0, end = 0)(1)
   
 
-    output$geo_sf_plot <- renderGirafe({
+    output$geo_sf_plot <- renderPlotly({
+      
+      max_n <- max(sf_interactive_df()$n, na.rm = TRUE)
       
       p <-
         ggplot(data = sf_interactive_df()) +
         geom_sf(data = static_df,
                 mapping = aes(), fill = map_fill_static_color, show.legend = F) +
-        geom_sf_interactive(aes(
-          tooltip = paste0(country,"- ", n, " languages \n\nList:\n", str_wrap(languages,45)),
-          fill = n,
-          data_id = country),
-          show.legend = T) +
+        geom_sf(aes(fill = n,
+                    text = str_wrap(paste0("Language families in <b>", country,"</b>: ", n,"<br><br>Languages include:\n", languages), 40)), show.legend = T) +
         scale_fill_viridis_c(trans = "sqrt", 
                              name = "Number of\nfamilies",
                              begin = 0.1,
                              labels = scales::label_number(accuracy = 1)) +
-        facet_wrap(~family) +
-        theme_void()
+        theme_void() +
+        theme(plot.title = element_text(colour = "white"),
+              legend.text = element_text(colour = "white"),
+              legend.title = element_text(colour = "white"))
       
-      girafe(ggobj = p)
+      ggplotly(p, tooltip = "text") %>%
+        layout(plot_bgcolor = "rgba(0,0,0,0.1)")
       
     })
 }
